1c73b215e5e7a70297d75cf0c3da16b80f7c2cfb
sedoid <karlsedoide@gmail.com>
2022-12-09 18:29:00 -0800

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,15 +1,20 @@
-#include "holberton.h"
+#include "main.h"
 
 
 
 /**
  *
- * create_file - creates a file
- * @filename: filename.
- * @text_content: content writed in the file.
- * Return: 1 if it success. -1 if it fails.
+ *  * create_file - creates a file
  *
- **/
+ *   * @filename: filename.
+ *
+ *    * @text_content: content writed in the file.
+ *
+ *     *
+ *
+ *      * Return: 1 if it success. -1 if it fails.
+ *
+ *       */
 
 int create_file(const char *filename, char *text_content)
 
